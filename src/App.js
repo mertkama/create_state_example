05f0b441ec0739299b33ce95d4701558fcd4d011
DESKTOP-VHLAITO\mert1 <mert172317@gmail.com>
2022-06-25 01:25:32 +0300

    input component added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Input from "./components/Input/Input";
 
 function App() {
+  const [text,setText] = useState("Mert")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {text}
+      <Input value= {text} placeholder={"Bir Metin İfadesi Giriniz..."} onChange = {(e)=> {
+        setText(e.target.value) 
+      }}/>
     </div>
   );
 }
