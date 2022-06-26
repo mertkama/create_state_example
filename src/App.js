702acbf544b6a813e5ab9a11b6d911c905dd98e5
DESKTOP-VHLAITO\mert1 <mert172317@gmail.com>
2022-06-26 21:31:42 +0300

    Demo App Published

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
-
+import Form from "./components/Form/Form"
+import './App.css'
 function App() {
-  const [text,setText] = useState("Mert")
+  const [text, setText] = useState("Mert")
   return (
-    <div >
-      {text}
-      <Input value= {text} placeholder={"Bir Metin İfadesi Giriniz..."} onChange = {(e)=> {
-        setText(e.target.value) 
-      }}/>
+    <div className="custom-page">
 
-      <Button text ="Giriş Yap" onClick={() => {alert("Tıklandı")}} />
+      <Form>
+        <h2>Giriş Sayfası </h2>
+        <Input value={text} placeholder={"Bir Metin İfadesi Giriniz..."} onChange={(e) => {
+          setText(e.target.value)
+        }} />
+
+        <Input value={text} placeholder={"Bir Metin İfadesi Giriniz..."} onChange={(e) => {
+          setText(e.target.value)
+        }} />
+
+        <Button text="Giriş Yap" onClick={() => { alert("Tıklandı") }} />
+      </Form>
+
     </div>
   );
 }
